b2b13f2dd3d2594ea74974bf52a65804b335b8e5
Jagrajan Bhullar <jag@jagrajan.com>
2021-06-20 22:50:35 -0700

    [terraform] add logging for pre-token-generation lambda

--- backend/terraform/lambda.tf
+++ backend/terraform/lambda.tf
@@ -13,6 +13,11 @@ resource "aws_lambda_function" "pre_token_generation" {
   runtime       = "nodejs14.x"
   s3_bucket     = aws_s3_bucket.lambdas.bucket
   s3_key        = data.aws_s3_bucket_object.pre_token_generation.key
+
+  depends_on = [
+    aws_cloudwatch_log_group.pre_token_generation,
+    aws_iam_role_policy_attachment.pre_token_generation_logging
+  ]
 }
 
 resource "aws_iam_role" "pre_token_generation" {
@@ -33,5 +38,33 @@ data "aws_iam_policy_document" "pre_token_generation" {
       identifiers = ["lambda.amazonaws.com"]
       type        = "Service"
     }
+  }
+}
+
+resource "aws_cloudwatch_log_group" "pre_token_generation" {
+  name              = "/aws/lambda/${var.stack_name}-pre-token-generation"
+  retention_in_days = 14
+}
+
+resource "aws_iam_role_policy_attachment" "pre_token_generation_logging" {
+  policy_arn =aws_iam_policy.pre_token_generation_logging.arn
+  role = aws_iam_role.pre_token_generation.name
+}
+
+resource "aws_iam_policy" "pre_token_generation_logging" {
+  policy = data.aws_iam_policy_document.pre_token_generation_logging.json
+}
+
+data "aws_iam_policy_document" "pre_token_generation_logging" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    effect = "Allow"
+    resources = [
+      "${aws_cloudwatch_log_group.pre_token_generation.arn}:*"
+    ]
   }
 }
