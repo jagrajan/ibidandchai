ed88e59be3163bc6d06df168c29442fa28f8f4d1
Jagrajan Bhullar <jag@jagrajan.com>
2021-06-20 22:51:02 -0700

    [lambda] pre token generation sets roles based on cognito groups

--- backend/lambda/hasura-pre-token-generation/src/index.ts
+++ backend/lambda/hasura-pre-token-generation/src/index.ts
@@ -1,13 +1,15 @@
 import { Handler, BasePreTokenGenerationTriggerEvent } from 'aws-lambda';
 
 export const handler: Handler<BasePreTokenGenerationTriggerEvent<'PreAuthentication_Authentication'>> = (event, context, callback) =>  {
+  const roles = [...(event.request.groupConfiguration?.groupsToOverride || []), 'user'];
+  const defaultRole = roles.find((role) => ['admin'].includes(role)) || 'user';
   event.response = {
     claimsOverrideDetails: {
       claimsToAddOrOverride: {
         "https://hasura.io/jwt/claims": JSON.stringify({
           "x-hasura-user-id": event.request.userAttributes.sub,
-          "x-hasura-default-role": "user",
-          "x-hasura-allowed-roles": ["user"],
+          "x-hasura-default-role": defaultRole,
+          "x-hasura-allowed-roles": roles,
         })
       }
     }
