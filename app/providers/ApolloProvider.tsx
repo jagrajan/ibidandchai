675796c960ab7b31d308618fc9b50f65c36dd7d1
Jagrajan Bhullar <jag@jagrajan.com>
2021-09-06 19:13:24 -0700

    [app] apollo should get latest auth state

--- app/providers/ApolloProvider.tsx
+++ app/providers/ApolloProvider.tsx
@@ -1,25 +1,25 @@
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, ApolloLink, concat } from '@apollo/client'
 import React from 'react'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:8080/v1/graphql',
 })
 
-const authLink = setContext((_, { headers }) => {
+const authMiddleware = new ApolloLink((operation, forward) => {
   // get the authentication token from local storage if it exists
   const token = localStorage.getItem('id_token')
   // return the headers to the context so httpLink can read them
-  return {
+  operation.setContext(({ headers = {} }) => ({
     headers: {
       ...headers,
-      ...(token ? { authorization: `Bearer ${token}` } : {}),
+      Authorization: `Bearer ${token}`,
     },
-  }
+  }))
+  return forward(operation)
 })
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: concat(authMiddleware, httpLink),
   cache: new InMemoryCache(),
 })
 
